e0bf2715e1073ede99b2122bc17eb17cd65daf92
Ezequiel7777 <ezequielworkout@gmail.com>
2022-10-31 11:14:43 -0300

    Entrega Final

--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -2,32 +2,39 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useCart } from "../context/cartContext";
 const Ticket = () => {
-    const { orders, clearOrder, clearCart} = useCart();
+  const { orders, products, clearOrder, clearCart } = useCart();
 
-    const clearHandler = () => {
-        clearOrder()
-        clearCart()
-    }
+  const clearHandler = () => {
+    clearOrder();
+    clearCart();
+  };
 
-    const style = {
-        width: "500px",
-        height: "300px",
-        backgroundColor: "#00bf92"
-    }
+  const style = {
+    width: "500px",
+    height: "300px",
+    backgroundColor: "#00bf92",
+  };
 
-    return (
-        <div style={style}>
-            <h1>Su orden de compra:</h1>
-        {
-            orders.map((order, i) => {
-                return <div key={i}>
-                <li> {order.title} </li>
-                <li> {order.price} </li>
-                <li> {order.quantity} </li>
-                </div>
-            })
-        } 
-        <Link to="/">
+  return (
+    <div style={style}>
+      <h1>Su orden de compra:</h1>
+      {orders.map((order, i) => {
+        return (
+          <div key={i}>
+            <li> Producto: {order.title} </li>
+            <li> Precio por unidad: {order.price} </li>
+            <li> Cantidad comprada: {order.quantity} </li>
+          </div>
+        );
+      })}
+      {products.map((product, q) => {
+        return (
+          <div key={q}>
+            <li> Precio total: {product.price * product.quantity} </li>
+          </div>
+        );
+      })}
+      <Link to="/">
         <button
           type="submit"
           name="submit"
@@ -36,9 +43,9 @@ const Ticket = () => {
         >
           Finalizar Compra
         </button>
-        </Link>
-        </div>
-    )
-}
+      </Link>
+    </div>
+  );
+};
 
-export default Ticket
+export default Ticket;
